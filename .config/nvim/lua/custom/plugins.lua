c9565d22dfc69c66486aedbafbe5a7e9610bfe47
Ashish Kumar <ashishkumar901336@gmail.com>
2024-10-31 18:28:05 +0530

    feat: adding extra features for go in nvim

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -52,6 +52,16 @@ local plugins = {
       }
       return opts
     end
-  }
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
 }
 return plugins
