3835d5b9c1e7241e63872e8f46a5f60a47dd3a3a
Ashish Kumar <ashishkumar901336@gmail.com>
2025-02-16 20:52:29 +0530

    fix: nvim symlink

---
+++ .config/nvim-bak/lua/custom/chadrc.lua
@@ -0,0 +1,10 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+  theme = 'catppuccin',
+}
+
+M.plugins = "custom.plugins"
+
+return M
---
+++ .config/nvim-bak/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,40 @@
+local base = require("plugins.configs.lspconfig")
+local on_attach = base.on_attach
+local capabilities = base.capabilities
+
+local lspconfig = require("lspconfig")
+local util = require "lspconfig/util"
+
+local servers = {"ts_ls", "tailwindcss", "eslint", "cssls"}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+lspconfig.clangd.setup {
+  on_attach = function (client, bufnr)
+    client.server_capabilities.signatureHelpProvider = false
+    on_attach(client, bufnr)
+  end,
+  capabilities = capabilities,
+}
+
+lspconfig.gopls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = {"gopls"},
+  filetypes = {"go", "gomod", "gowork", "gotmpl"},
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+      },
+    },
+  },
+}
