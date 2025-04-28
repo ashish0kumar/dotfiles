cfb93d55656e679893386ebdca1523f3183b53d5
Ashish Kumar <ashishkumar901336@gmail.com>
2025-04-29 00:09:08 +0530

    nvim

---
+++ .config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,13 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+  theme = 'catppuccin',
+  hl_override = {
+    IndentBlanklineChar = { fg = "#1e1d2d" },
+  },
+}
+
+M.plugins = "custom.plugins"
+
+return M
